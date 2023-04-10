50d8a20a3824e0364bdb6827fda7ea672158cddc
赵梁煊 <zhaoliangxuan@zhaoliangxuandeMacBook-Pro16.local>
2023-04-10 21:49:29 +0800

    format(zlx): modify train_rm.sh

--- applications/Chat/examples/train_rm.sh
+++ applications/Chat/examples/train_rm.sh
@@ -1,8 +1,32 @@
+set_n_least_used_CUDA_VISIBLE_DEVICES() {
+    local n=${1:-"9999"}
+    echo "GPU Memory Usage:"
+    local FIRST_N_GPU_IDS=$(nvidia-smi --query-gpu=memory.used --format=csv \
+        | tail -n +2 \
+        | nl -v 0 \
+        | tee /dev/tty \
+        | sort -g -k 2 \
+        | awk '{print $1}' \
+        | head -n $n)
+    export CUDA_VISIBLE_DEVICES=$(echo $FIRST_N_GPU_IDS | sed 's/ /,/g')
+    echo "Now CUDA_VISIBLE_DEVICES is set to:"
+    echo "CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES"
+}
+
 set_n_least_used_CUDA_VISIBLE_DEVICES 1
 
+# single GPU
 python train_reward_model.py --pretrain 'microsoft/deberta-v3-large' \
                              --model 'deberta' \
                              --strategy naive \
                              --loss_fn 'log_exp'\
                              --save_path 'rmstatic.pt' \
-                             --test True
+                             --test False
+
+# multiple GPU
+# torchrun --standalone --nproc_per_node=4 train_reward_model.py --pretrain 'bigscience/bloomz-7b1' \
+#                              --model 'bloom' \
+#                              --strategy collosalai_zero2 \
+#                              --loss_fn 'log_exp'\
+#                              --save_path 'rmstatic.pt' \
+#                              --test False
